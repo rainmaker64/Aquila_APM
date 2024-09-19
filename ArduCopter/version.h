7fcca121fdb549523624a8bfcb895462eb482273
Seunghyun Jung <91441739+rainmaker64@users.noreply.github.com>
2024-09-19 11:23:14 +0900

    Change version to "AQUILA-2 V02.02.00P"

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "AQUILA-III V02.01.00"
+#define THISFIRMWARE "AQUILA-2 V02.02.00P"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,0,7,FIRMWARE_VERSION_TYPE_OFFICIAL
